9529cd720c52600c4a626187c8513133b1eee812
Lucas Panizio <lucas_panizio@hotmail.com>
2024-10-10 18:11:05 -0300

    :sparkles: feat: add new test for duplicated names

--- src/__tests__/Form.spec.tsx
+++ src/__tests__/Form.spec.tsx
@@ -1,37 +1,19 @@
+import { RecoilRoot } from 'recoil';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Form } from '@/components/Form';
-import { RecoilRoot } from 'recoil';
 
-test("New attendees can't be added when the input is empty", () => {
+const renderComponent = () => {
   render(
     <RecoilRoot>
       <Form />
     </RecoilRoot>,
   );
+};
 
-  const input = screen.getByPlaceholderText(
-    'Insira os nomes dos participantes',
-  );
-
-  const button = screen.getByRole('button');
-
-  expect(input).toBeInTheDocument();
-  expect(button).toBeDisabled();
-});
-
-test('New attendee can be added when the input is filled', () => {
-  render(
-    <RecoilRoot>
-      <Form />
-    </RecoilRoot>,
-  );
-
-  const input = screen.getByPlaceholderText(
-    'Insira os nomes dos participantes',
-  );
-
-  const button = screen.getByRole('button');
-
+const addNameInInputAndClickOnButton = (
+  input: HTMLElement,
+  button: HTMLElement,
+) => {
   fireEvent.change(input, {
     target: {
       value: 'Ana Catarina',
@@ -39,7 +21,50 @@ test('New attendee can be added when the input is filled', () => {
   });
 
   fireEvent.click(button);
+};
 
-  expect(input).toHaveFocus();
-  expect(input).toHaveValue('');
+describe('Form', () => {
+  it('Should not add a name to the list if the input is empty', () => {
+    renderComponent();
+
+    const input = screen.getByPlaceholderText(
+      'Insira os nomes dos participantes',
+    );
+
+    const button = screen.getByRole('button');
+
+    expect(input).toBeInTheDocument();
+    expect(button).toBeDisabled();
+  });
+
+  it('Should add a name to the list if the input is filled', () => {
+    renderComponent();
+
+    const button = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(
+      'Insira os nomes dos participantes',
+    );
+
+    addNameInInputAndClickOnButton(input, button);
+
+    expect(input).toHaveFocus();
+    expect(input).toHaveValue('');
+  });
+
+  it('Should not add duplicate names to the list', () => {
+    renderComponent();
+
+    const button = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(
+      'Insira os nomes dos participantes',
+    );
+
+    addNameInInputAndClickOnButton(input, button);
+    addNameInInputAndClickOnButton(input, button);
+
+    const errorMessage = screen.getByRole('alert');
+    expect(errorMessage.textContent).toBe(
+      'Nomes duplicados não são permitidos!',
+    );
+  });
 });
