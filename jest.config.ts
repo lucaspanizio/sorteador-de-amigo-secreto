e56193c70064e8e4719515e3e7a4d04cebc5f0af
Lucas Panizio <lucas_panizio@hotmail.com>
2024-10-09 21:55:37 -0300

    :package: build: update jest config and add react router dom

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,17 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Alias path
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  testMatch: [
+    '**/__tests__/**/*.test.(ts|tsx)',
+    '**/?(*.)+(spec|test).(ts|tsx)',
+  ],
 };
 
 export default config;
